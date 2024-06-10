ad9bc4fa69dc6b6c6a10b9faf6f208fa9dd77070
unknown <SharmilaPuligundla2002@gmail.com>
2024-06-10 11:49:53 +0530

    completed

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import {  Container, Stack } from '@chakra-ui/react'
+import TodoList from "./components/TodoList";
+
+  
+export const BASE_URL = import .meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api/";
+function App() {
+  return (
+    <Stack h="100vh">
+          <Navbar/>
+          <Container>
+            <TodoForm />
+            <TodoList/>
+          </Container>
+    </Stack>
+  );
+}
+
+export default App;
